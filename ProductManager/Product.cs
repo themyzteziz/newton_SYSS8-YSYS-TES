3c706fda7bda53cae6c702185d56d5f073b6138a
themyzteziz <markus.siteus@hotmail.com>
2026-03-26 14:58:39 +0100

    Change

---
+++ ProductManager/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductManager;
+
+public class Product
+{
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public string Price { get; set; } = "";
+}
---
+++ ProductManager/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+
+namespace ProductManager;
+
+public class ProductService
+{
+    public List<Product> GetProductsByCategory(string category)
+    {
+
+        var allProducts = new List<Product>();
+
+        using var connection = new NpgsqlConnection(
+            "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=productsdb");
+
+        connection.Open();
+
+        var command = new NpgsqlCommand(
+            "SELECT name, category, price FROM products",
+            connection);
+
+        command.Parameters.AddWithValue("@category", category);
+
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            allProducts.Add(new Product
+            {
+                Name = reader.GetString(0),
+                Category = reader.GetString(1),
+                Price = reader.GetString(2)
+            });
+        }
+        connection.Close();
+
+        return result;
+    }
+}
