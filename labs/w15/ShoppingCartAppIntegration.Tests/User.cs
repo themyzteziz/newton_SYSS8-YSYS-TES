856a8d2baad659e323818f8b01b667accbe887a2
Alejandro Rosero Vicuña <alejandroroserov@gmail.com>
2026-04-07 01:00:32 +0200

    Lab w15 added.

---
+++ labs/w15/ShoppingCartAppIntegration.Tests/User.cs
@@ -0,0 +1,29 @@
+﻿namespace ShoppingCartAppIntegration.Tests;
+
+using System.Net.Http;
+using System.Net.Http.Json;
+
+[TestClass]
+public class User
+{
+    private static readonly HttpClient client = new HttpClient();
+
+
+    [TestMethod]
+    public void RegisterNewCustomer()
+    {
+        // Hint: Use appUrl from GlobalContext to make API calls to the application
+        // GlobalContext.appUrl
+
+
+        //Implement tests
+        Assert.IsTrue(false, "Test not implemented yet.");
+    }
+
+    [TestMethod]
+    public void CustomerListsProductsInCart()
+    {
+        //Implement tests
+        Assert.IsTrue(false, "Test not implemented yet.");
+    }
+}
